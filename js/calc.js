b3cb6d388d751cf3b2590317534e0b80a7459ba1
roman <mromanek1802@gmail.com>
2022-05-19 22:32:58 +0200

    Bugs fix (operators on the begining)

--- js/calc.js
+++ js/calc.js
@@ -27,13 +27,17 @@ function operate(operator,n1,n2){
 }
 
 function assignNumber(n){
+
+    if(n1.length > 15 || n2.length > 15) return;
+    if(n1.length == 0 && n.textContent == ".") return;
+    if(n2.length == 0 && n.textContent == ".") return;
     
     if(operator==""){ 
-        if(n1.length>15) return;
+        if(n1.includes(".") && n.textContent == ".") return; 
         n1=n1+n.textContent;
         outputScreen.textContent = n1;
     }else{
-        if(n2.length>15) return;
+        if(n2.includes(".") && n.textContent == ".") return; 
         n2=n2+n.textContent;
         outputScreen.textContent = n2;
     }
@@ -46,6 +50,32 @@ function clearScreen(){
     operator = "";
     previousOperator = "";
     outputScreen.textContent = 0;
+}
+
+function calculate(o){
+    previousOperator = operator;
+    operator = o.id;
+    
+    if(operator == "="){
+        if(n1 == ""){
+            operator = "";
+            return;
+        } 
+        result = operate(previousOperator,n1,n2);
+        if(result % 1 != 0) result = result.toFixed(2);
+        
+    }else if(operator == "add" || operator == "divide" || operator == "sum" || operator == "subtract"){
+        operator = "";
+        return;
+    }else if(n2 != ""){
+        result = operate(operator,n1,n2);
+    }else{
+        return;
+    }
+
+    outputScreen.textContent = result;
+    n1 = result;
+    n2 = "";
 }
 
 let n1 = "";
@@ -69,28 +99,12 @@ numbers.forEach((n) => n.addEventListener("click", () => {
 }));
 
 let operators = document.querySelectorAll(".operator");
-operators.forEach((o) => o.addEventListener("click",() => {
-    previousOperator = operator;
-    operator = o.id;
+operators.forEach((o) => o.addEventListener("click", () => {
+    calculate(o);
     o.classList.add("click");
-
-    if(operator == ""){
-        result = operate(previousOperator,n1,n2);
-        if(result % 1 != 0) result = result.toFixed(2);
-    }else if(n2 !=""){
-        result = operate(operator,n1,n2);
-    }else{
-        return;
-    }
-
-    outputScreen.textContent = result;
-    n1 = result;
-    n2 = "";
-  
 }));
 
 let buttons = document.querySelectorAll("button");
 buttons.forEach((btn) => btn.addEventListener("transitionend", (event) => {
-
     if(event.propertyName!="transform") return;
     btn.classList.remove("click")}));
