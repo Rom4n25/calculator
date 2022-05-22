545a034374ee5757d6f80748c7ba739bafd84b7a
roman <mromanek1802@gmail.com>
2022-05-22 13:09:59 +0200

    Fix bug, add Del button.

--- js/calc.js
+++ js/calc.js
@@ -15,74 +15,122 @@ function divide(n1,n2){
 }
 
 function operate(operator,n1,n2){
-    let numb1 = parseFloat(n1);
-    let numb2 = parseFloat(n2);
+    const firstNumber = parseFloat(n1);
+    const secondNumber = parseFloat(n2);
 
     switch(operator){
-        case "add" : return sum(numb1,numb2);
-        case "subtract" : return subtract(numb1,numb2);
-        case "multiply" : return multiply(numb1,numb2);
-        case "divide" : return divide(numb1,numb2);
+        case "+" : return sum(firstNumber,secondNumber);
+        case "-" : return subtract(firstNumber,secondNumber);
+        case "*" : return multiply(firstNumber,secondNumber);
+        case "/" : return divide(firstNumber,secondNumber);
     }
 }
 
 function assignNumber(n){
+    const currentNumber = n.textContent;
 
-    if(n1.length > 15 || n2.length > 15) return;
-    if(n1.length == 0 && n.textContent == ".") return;
-    if(n2.length == 0 && n.textContent == ".") return;
+    if(firstNumber.length > 15 || secondNumber.length > 15) return;
+    if(firstNumber.length == 0 && currentNumber == "."){
+        firstNumber = 0 + n.textContent;
+        outputScreen.textContent = firstNumber;
+    };
+    
+    if(operator == "="){
+        if(currentNumber == ".") return; 
+        clearScreen();
+        firstNumber =  currentNumber;
+        outputScreen.textContent = firstNumber;
 
-    if(operator==""){ 
-        if(n1.includes(".") && n.textContent == ".") return; 
-        n1=n1+n.textContent;
-        outputScreen.textContent = n1;
+    }else if(operator == ""){ 
+        if(firstNumber.includes(".") && currentNumber == ".") return; 
+        firstNumber = firstNumber + currentNumber;
+        outputScreen.textContent = firstNumber;
+
     }else{
-        if(n2.includes(".") && n.textContent == ".") return; 
-        n2=n2+n.textContent;
-        outputScreen.textContent = n2;
+        if(secondNumber.length == 0 && currentNumber == "."){
+            secondNumber = 0 + n.textContent;
+            outputScreen.textContent += secondNumber;
+        };
+        if(secondNumber.includes(".") && currentNumber == ".") return; 
+        secondNumber = secondNumber + currentNumber;
+        outputScreen.textContent += currentNumber;
     }
 }
 
 function clearScreen(){
-    n1 = "";
-    n2 = "";
+    firstNumber = "";
+    secondNumber = "";
     result = "";
     operator = "";
     previousOperator = "";
     outputScreen.textContent = 0;
 }
 
-function assignVariables(o){
-    previousOperator = operator;
+function assignOperator(o){
+    if(firstNumber == "") return;
+
+    const previousOperator = operator;
     operator = o.id;
+
+    const allOperators = "+-/*="
+    const lastOutputScreenSymbol = outputScreen.textContent.charAt(outputScreen.textContent.length-1);
     
-    if(operator == "="){
-        if(n1 == ""){
-            operator = "";
-            return;
-        } 
-        result = operate(previousOperator,n1,n2);
-        if(result % 1 != 0) result = result.toFixed(2);
-        
-    }else if(n2 != ""){
-        result = operate(previousOperator,n1,n2);
-    }else if(operator == "add" || operator == "divide" || operator == "sum" || operator == "subtract"){
-        operator = "";
-        return;
+    if(!allOperators.includes(lastOutputScreenSymbol) ){
+        if(operator != "="){
+            outputScreen.textContent += operator;
+        }
     }else{
-        return;
+        if(operator != "="){
+        outputScreen.textContent = outputScreen.textContent.slice(lastOutputScreenSymbol,this.length-1) + operator;
+        }else{
+            clearScreen();
+        }
     }
 
-    outputScreen.textContent = result;
-    n1 = result;
-    n2 = "";
+    if(secondNumber == "") return;
+
+    if(operator == "="){
+        result = operate(previousOperator,firstNumber,secondNumber);
+        if(result % 1 != 0) result = result.toFixed(2);
+        outputScreen.textContent = result;
+       
+    }else{
+        result = operate(previousOperator,firstNumber,secondNumber);
+    }
+
+    firstNumber = result;
+    secondNumber = "";
 }
 
-let n1 = "";
-let n2 = "";
+function deleteLastInput(){
+    const allOperators = "+-/*"
+    let lastOutputScreenSymbol = outputScreen.textContent.charAt(outputScreen.textContent.length-1);
+
+    if(outputScreen.textContent.length>1){
+        outputScreen.textContent = outputScreen.textContent.slice(0,this.length-1);
+    }else{
+        clearScreen();
+    }
+
+    if(allOperators.includes(lastOutputScreenSymbol)){
+        operator = "";
+    }else{
+        if(operator == "" || operator == "="){
+            firstNumber = firstNumber.toString();
+            firstNumber = firstNumber.slice(0,firstNumber.length-1);
+            console.log(firstNumber);
+            
+        }else{
+            secondNumber = secondNumber.slice(0,secondNumber.length-1);
+            console.log(secondNumber);
+        }
+    }
+}
+
+let firstNumber = "";
+let secondNumber = "";
 let result = 0;
 let operator = "";
-let previousOperator = "";
 
 let outputScreen = document.querySelector(".result");
 
@@ -90,6 +138,12 @@ let clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click",() => {
     clearScreen();
     clearBtn.classList.add("click");
+});
+
+let deleteBtn = document.querySelector(".delete");
+deleteBtn.addEventListener("click",() => {
+    deleteLastInput();
+    deleteBtn.classList.add("click");
 });
 
 let numbers = document.querySelectorAll(".number");
@@ -100,7 +154,7 @@ numbers.forEach((n) => n.addEventListener("click", () => {
 
 let operators = document.querySelectorAll(".operator");
 operators.forEach((o) => o.addEventListener("click", () => {
-    assignVariables(o);
+    assignOperator(o);
     o.classList.add("click");
 }));
 
